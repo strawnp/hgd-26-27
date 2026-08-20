c14feeffa5edd626fb86a1c0290ff354f52173ab
Peter Strawn <StrawnP@Mac.JHS.DOMAIN>
2026-08-20 11:33:00 -0700

    pong-3

--- pong/main.lua
+++ pong/main.lua
@@ -6,10 +6,13 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+PADDLE_SPEED = 200
+
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
     smallFont = love.graphics.newFont('font.ttf', 8)
+    scoreFont = love.graphics.newFont('font.ttf', 32)
 
     love.graphics.setFont(smallFont)
 
@@ -22,6 +25,28 @@ function love.load()
     push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, {
         upscale = 'normal'
     })
+
+    player1Score = 0
+    player2Score = 0
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
+end
+
+function love.update(dt)
+    -- player 1 movement
+    if love.keyboard.isDown('w') then 
+        player1Y = player1Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('s') then 
+        player1Y = player1Y + PADDLE_SPEED * dt 
+    end
+
+    -- player 2 movement
+    if love.keyboard.isDown('up') then 
+        player2Y = player2Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('down') then 
+        player2Y = player2Y + PADDLE_SPEED * dt 
+    end
 end
 
 function love.keypressed(key)
@@ -35,19 +60,18 @@ function love.draw()
 
     love.graphics.clear(40/255, 45/255, 52/255, 255/255)
 
-    love.graphics.printf(
-        'Hello Pong!',
-        0,
-        20,
-        VIRTUAL_WIDTH,
-        'center'
-    )
+    love.graphics.setFont(smallFont)
+    love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
 
     -- left paddle
-    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
 
     -- right paddle
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, VIRTUAL_HEIGHT - 50, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, player2Y, 5, 20)
 
     -- ball
     love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT/ 2 - 2, 4, 4)
